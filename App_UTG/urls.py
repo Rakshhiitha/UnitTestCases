048c7103615c09c9ab80c07c1f745e1b0f896cf5
Rakshitha N <N.Rakshitha@sonata-software.com>
2025-04-25 15:02:29 +0530

    Remove all files from repository

--- App_UTG/urls.py
+++
@@ -1,9 +0,0 @@
-# App_UTG/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.hello_world, name='hello_world'),  # Root URL
-    path('generate/', views.generate_tests, name='generate_tests'),  # Endpoint for generating tests
-]
